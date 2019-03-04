474443bd31d5b7c2f50481e1aa9c070b4ce4bd90
Mohammad Matini <mohammad.matini@outlook.com>
2019-03-04 09:51:17 +0300

    Add File License Notices

--- src/Feegra.php
+++ src/Feegra.php
@@ -1,6 +1,28 @@
 #!/usr/bin/env php
 <?php namespace Feegra;
 
+
+// Copyright (C) 2019 Mohammad Matini
+
+// Author: Mohammad Matini <mohammad.matini@outlook.com>
+// Maintainer: Mohammad Matini <mohammad.matini@outlook.com>
+
+// This file is part of Feegra.
+
+// Feegra is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Feegra is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Feegra.  If not, see <https://www.gnu.org/licenses/>.
+
+
 use GetOpt\GetOpt;
 use GetOpt\Option;
 use GetOpt\Command;
@@ -21,6 +43,9 @@ echo
  #mmmmm #"  #  #"  #  #   mm  #"  " "   #
  #      #""""  #""""  #    #  #     m"""#
  #      "#mm"  "#mm"   "mmm"  #     "mm"#
+
+ Welcome to Feegra, the Facebook Feed Grabber.
+ ---------------------------------------------
 
 ';
 
